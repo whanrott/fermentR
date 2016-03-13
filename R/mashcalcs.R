152f4b1125f165ba0a12a1995eb2aabd1ddf8040
whanrott <eart0208@nexus.ox.ac.uk>
2016-03-13 09:17:36 +0000

    minor corrections to mash calculations

--- R/mashcalcs.R
+++ R/mashcalcs.R
@@ -12,20 +12,19 @@
 # shw: specific heat of water in kJ/KgC. Defaults to 1.0kJ/KgC
 # shg: specific heat of grain in kJ/KgC. Defaults to 0.4kJ/KgC
 
-# > mashtemp(5,5,80,20)
+# > mashtemp(5,5,NA,80,20) will calculate the mash temperature
 # [1] 62.85714
-# > strike(5,5,62,20)
+# > mashtemp(5,5,62,NA,20) will calculate strike temperature
 # [1] 78.8
 
 
 mashtemp <- function(ww, wg, tm, tw, tg, shw = 1.0,shg = 0.4) {
-  print(is.na(c(ww,wg,tm, tw,tg,shw,shg)))
   if(sum(is.na(c(ww,wg,tm, tw,tg,shw,shg))) > 1) {
     ## put error message here: too many unassigned variables
     } else if(is.na(tm) == T) {
     ## calculate mash temperature
     return(((shw*ww*tw)+(shg*wg*tg))/((shw*ww)+(shg*wg)))
-    } else if (is.na(tm) == T) {
+    } else if (is.na(tw) == T) {
     ## calculate strike temperature (ie water temp)
       return((tm*shw*ww+(tm*shg-shg*tg)*wg)/(shw*ww))
     } else if (is.na(ww)) {
