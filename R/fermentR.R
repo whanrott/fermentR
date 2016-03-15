8bf23019ff210f6f4a00e064025ae08c67c65895
whanrott <eart0208@nexus.ox.ac.uk>
2016-03-15 06:10:35 +0000

    restructured mash_calcs, merged strike

---
+++ R/fermentR.R
@@ -0,0 +1,31 @@
+## General documentation for fermentR package
+
+#' fermentR: A package of converters and calculators for brewers of beer and
+#' other fermented drinks.
+#'
+#' This is a placeholder description
+#'
+#' The fermentR package covers areas of interest to brewers:
+#'
+#' Converting between units of gravity (which measure concentration of sugars in water).
+#' Calculating useful characteristics water chemistry and suggesting treatment (planned).
+#' Calculating composition and temperature of mashes.
+#' Calculating usage and effect of hops.
+#' Calculating amounts of alcohol in fermented worts.
+#' Calculating gas volumes in fermented worts and suggesting gas line lengths to control dispense (planned).
+#'
+#' @section some more text:
+#'  in this section...
+#'
+#' @param Adjunct z
+#' @param Grist The mix of grains and adjuncts which will be combined with liquor in the mash.
+#' @param Gyle z
+#' @param Liquor Water which will be used during the brewing process.
+#' @param Malt y
+#' @param Mash x
+#' @param Sparge y
+#' @param Wort x
+#'
+#' @docType package
+#' @name fermentR
+NULL
