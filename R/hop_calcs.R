025a976065119824b221cc6afa7881bcbf18769f
whanrott <eart0208@nexus.ox.ac.uk>
2016-03-19 21:27:07 +0000

    added hop utilization calculation

--- R/hop_calcs.R
+++ R/hop_calcs.R
@@ -16,33 +16,35 @@
 #' hop_calcs(mh = NA, vw = 21, ibu = 25, aa = 0.103, utilization = 0.30)
 #' # calculate the weight of hops to achieve 25 IBU using Cascade at 10.3% Alpha Acids and 30 % utilization
 #' hop_weight(21, 25, 0.103, 0.30)
+#' # calculate the ibu of using 17g Cascade hops at 10.3% Alpha Acids and 30 % utilization
+#' ibu(mh = 17, vw = 21, aa = 0.103, utilization = 0.30)
+#' # calculate the hop utilization
+#' hop_utilization(mh = 17, vw = 21, ibu = 25, aa = 0.103)
 #' @references
 #' Lewis MJ, Young, TW. (2001). Brewing, 2nd Edition. Klewer Academic / Plenum Publishers
 #' @export
 hop_calcs <- function(mh = NA, vw = NA, ibu = NA, aa = NA, utilization = NA) {
-  # weight of hops = (wort volume * desired IBU)/(% Alpha Acids * % utilization)
-  # mh = (vw*ibu)/(aa*utilization)
-  # vw=(aa*mh*utilization)/ibu
-  # ibu=(aa*mh*utilization)/vw
-  # aa=(ibu*vw)/(mh*utilization)
-  # utilization=(ibu*vw)/(aa*mh)
-
-  if (sum(is.na(c(vw,ibu,aa,utilization))) > 1) {
-    ## put error message here: too many unassigned variables
-  } else if (is.na(mh) == T) {
-    return((vw*ibu)/(aa*utilization*1000))
-  } else if (is.na(vw) == T) {
-    return((aa*mh*utilization*1000)/ibu)
-  } else if (is.na(ibu) == T) {
-    return((aa*mh*utilization*1000)/vw)
-  } else if (is.na(aa) == T) {
-    return((ibu*vw)/(mh*utilization*1000))
-  } else if (is.na(utilization) == T) {
-    return((ibu*vw)/(aa*mh*1000))
-  } else {
-    return(NA)
+  # mass of hops = (wort volume * desired IBU)/(% Alpha Acids * % utilization)
+  tmp <- is.na(list(mh, vw, ibu, aa, utilization))
+  if (sum(tmp) == 0) {
+    stop("You can't specify all three values")
   }
-}
+  if (sum(tmp) > 1) {
+    stop("Too many values specified")
+  }
+  switch( which(is.na(list(mh, vw, ibu, aa, utilization))),
+          # mh = (vw*ibu)/(aa*utilization)
+          return((vw*ibu)/(aa*utilization*1000)),
+          # vw=(aa*mh*utilization)/ibu
+          return((aa*mh*utilization*1000)/ibu),
+          # ibu=(aa*mh*utilization)/vw
+          return((aa*mh*utilization*1000)/vw),
+          # aa=(ibu*vw)/(mh*utilization)
+          return((ibu*vw)/(mh*utilization*1000)),
+          # utilization=(ibu*vw)/(aa*mh)
+          return((ibu*vw)/(aa*mh*1000))
+          )
+  }
 
 #' @rdname hop_calcs
 #' @export
@@ -54,4 +56,10 @@ hop_weight <- function(vw, ibu, aa,  utilization) {
 #' @export
 ibu <- function(mh, vw, aa, utilization) {
   return(hop_calcs(mh, vw, ibu = NA, aa,  utilization))
+}
+
+#' @rdname hop_calcs
+#' @export
+hop_utilization <- function(mh, vw, ibu, aa) {
+  return(hop_calcs(mh, vw, ibu, aa, utilization = NA))
 }
